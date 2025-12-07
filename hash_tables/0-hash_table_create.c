c2a0f62c161d1864bf5fe5f4cc7ecaae4794d91b
Denis12306 <denis.gavaud@gmail.com>
2025-12-07 10:45:50 +0100

    Changes

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -8,20 +8,26 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	unsigned long int i = 0;
-	hash_table_t *new_table = NULL;
+	hash_table_t *new_table;
+	unsigned long int i;
+
+	if (size == 0)
+		return (NULL);
 
 	new_table = malloc(sizeof(hash_table_t));
-	if (!new_table)
+	if (new_table == NULL)
 		return (NULL);
+
 	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
-	if (!new_table->array)
+	if (new_table->array == NULL)
 	{
 		free(new_table);
 		return (NULL);
 	}
-	for (; i < size; i++)
-		(new_table->array)[i] = NULL;
+
+	for (i = 0; i < size; i++)
+		new_table->array[i] = NULL;
+
 	return (new_table);
 }
