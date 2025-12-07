fd63a699d61b5175603ae54f52f5e4cc5e2c3987
Denis12306 <denis.gavaud@gmail.com>
2025-12-07 11:12:41 +0100

    Modif

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -11,44 +11,51 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index = 0;
-	char *value_dup = NULL, *key_dup = NULL;
-	hash_node_t *new_node = NULL, *tmp_node = NULL;
+    unsigned long int index;
+    char *key_dup, *value_dup;
+    hash_node_t *new_node, *tmp;
 
-	if (!ht || !key || !value)
-		return (0);
-	else if (strlen(key) == 0)
-		return (0);
-	value_dup = strdup(value);
-	key_dup = strdup(key);
-	new_node = malloc(sizeof(hash_node_t));
-	if (!new_node)
-		return (0);
-	new_node->key = key_dup;
-	new_node->value = value_dup;
-	new_node->next = NULL;
-	index = key_index((unsigned char *)key, ht->size);
-	if ((ht->array)[index] != NULL)
-	{
-		tmp_node = (ht->array)[index];
-		while (tmp_node)
-		{
-			if (strcmp(tmp_node->key, key_dup) == 0)
-			{
-				free(tmp_node->value);
-                tmp_node->value = value_dup;
+    if (!ht || !key || !value || strlen(key) == 0)
+        return (0);
 
-				free(key_dup);
-				free(new_node);
-				return (1);
-			}
-			tmp_node = tmp_node->next;
-		}
-		tmp_node = (ht->array)[index];
-		new_node->next = tmp_node;
-		(ht->array)[index] = new_node;
-	}
-	else
-		(ht->array)[index] = new_node;
-	return (1);
+    key_dup = strdup(key);
+    value_dup = strdup(value);
+    if (!key_dup || !value_dup)
+    {
+        free(key_dup);
+        free(value_dup);
+        return (0);
+    }
+
+    new_node = malloc(sizeof(hash_node_t));
+    if (!new_node)
+    {
+        free(key_dup);
+        free(value_dup);
+        return (0);
+    }
+
+    new_node->key = key_dup;
+    new_node->value = value_dup;
+    new_node->next = NULL;
+
+    index = key_index((const unsigned char *)key, ht->size);
+
+    tmp = ht->array[index];
+    while (tmp)
+    {
+        if (strcmp(tmp->key, key) == 0)
+        {
+            free(tmp->value);
+            tmp->value = value_dup;
+            free(key_dup);
+            free(new_node);
+            return (1);
+        }
+        tmp = tmp->next;
+    }
+
+    new_node->next = ht->array[index];
+    ht->array[index] = new_node;
+    return (1);
 }
