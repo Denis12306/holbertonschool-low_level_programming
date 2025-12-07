89aafd8332e5b2366e79e33410fcc76df1c42288
Denis12306 <denis.gavaud@gmail.com>
2025-12-07 11:02:34 +0100

    Modification

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -8,8 +8,8 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int index = 0;
+	if (key == NULL || size == 0)
+		return (0);
 
-	index = hash_djb2((unsigned char *)key);
-	return (index % size);
+	return (hash_djb2(key) % size);
 }
