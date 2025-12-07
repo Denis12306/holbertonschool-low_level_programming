f80e6c88834c8a709bca04ca48a70f2cf7d718ea
Denis12306 <denis.gavaud@gmail.com>
2025-12-07 11:15:54 +0100

    Modification

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -9,25 +9,20 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index = 0;
-	char *value = NULL;
-	hash_node_t *node;
+    unsigned long int index;
+    hash_node_t *node;
 
-	if (!ht || !key)
-		return (NULL);
-	else if (strlen(key) == 0)
-		return (NULL);
+    if (!ht || !key || strlen(key) == 0)
+        return NULL;
 
-	index = key_index((unsigned char *)key, ht->size);
-	if ((ht->array)[index] == NULL)
-		return (NULL);
+    index = key_index((const unsigned char *)key, ht->size);
 
-	node = ht->array[index];
-	while (!value)
-	{
-		if (strcmp(node->key, key) == 0)
-			value = node->value;
-		node = node->next;
-	}
-	return (value);
+    node = ht->array[index];
+    while (node)
+    {
+        if (strcmp(node->key, key) == 0)
+            return node->value;
+        node = node->next;
+    }
+    return NULL;
 }
